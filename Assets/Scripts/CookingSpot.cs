b295c65ef89c6bfb651cbd121c5c7e9a8ca0706a
Elisha <elisha@esarkis.ca>
2023-02-03 21:25:11 -0500

    Proof of concept cooking - changes material but that is all, and janky

---
+++ Assets/Scripts/CookingSpot.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookingSpot : MonoBehaviour
+{
+    [Header("Asset References")]
+    public List<GameObject> fireObjects;
+    public List<GameObject> mushroomObjects;
+    public Material cookedMushroom;
+
+    [Header("Parameters")]
+    public Vector3 fireOffset;
+    public float cookTimer = 5.0f;
+
+    bool _isCooking = false;
+    
+    void Start()
+    {
+        foreach (var fire in fireObjects)
+        {
+            fire.SetActive(false);
+        }
+    }
+    
+    void Update()
+    {
+        if (_isCooking)
+        {
+            for (int i = 0; i < mushroomObjects.Count; i++)
+            {
+                fireObjects[i].transform.position = mushroomObjects[i].transform.position + fireOffset;
+                fireObjects[i].transform.parent = mushroomObjects[i].transform;
+            }
+        }
+    }
+
+    void StartCooking()
+    {
+        _isCooking = true;
+        foreach (var fire in fireObjects)
+        {
+            fire.SetActive(true);
+        }
+        StartCoroutine(BurningTimer());
+    }
+
+    void OnBurningEnd()
+    {
+        _isCooking = false;
+        foreach (var fire in fireObjects)
+        {
+            fire.SetActive(false);
+        }
+
+        foreach (var mushroom in mushroomObjects)
+        {
+            foreach (var meshRenderer in mushroom.GetComponentsInChildren<MeshRenderer>())
+            {
+                meshRenderer.material = cookedMushroom;
+            }
+        }
+    }
+
+    IEnumerator BurningTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(cookTimer);
+            OnBurningEnd();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Mushroom") && !_isCooking)
+        {
+            mushroomObjects.Add(other.gameObject);
+            StartCooking();
+        }
+    }
+}
